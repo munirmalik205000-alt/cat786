2beba34415d5b56dc36c81f085970622aac40c53
munirmalik205000-alt <munir.malik205000@gmail.com>
2026-04-24 21:16:53 +0530

    Update firebase_config.py

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -1,9 +1,11 @@
 import firebase_admin
-from firebase_admin import credentials, db
+from firebase_admin import credentials
 import os
+import json
 
-cred = credentials.Certificate("serviceAccountKey.json")
+firebase_key = json.loads(os.environ["FIREBASE_KEY"])
 
+cred = credentials.Certificate(firebase_key)
 firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://ridabest786-default-rtdb.firebaseio.com"
+    "databaseURL": "https://ridabest786-default-rtdb.firebaseio.com"
 })
